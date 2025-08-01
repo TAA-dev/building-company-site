84a60a97024cf69430ed4e96f7bea2afe07a6045
Anton Tkachuk <tkachuk.ant@mail.ru>
2025-08-01 13:03:53 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,194 +1,164 @@
-const path = {
-  build: {
-    html: 'build/',
-    style: 'build/style/',
-    fonts: 'build/fonts/',
-    script: 'build/js/',
-    img: 'build/img/',
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import cleanCSS from 'gulp-clean-css';
+import imagemin from 'gulp-imagemin';
+import browserSync from 'browser-sync';
+import autoprefixer from 'gulp-autoprefixer';
+import sourcemaps from 'gulp-sourcemaps';
+import { deleteAsync } from 'del';
+import fileInclude from 'gulp-file-include';
+import mozjpeg from 'imagemin-mozjpeg'; // Оптимизация JPEG
+import pngquant from 'imagemin-pngquant'; // Оптимизация PNG
+import gifsicle from 'imagemin-gifsicle'; // Оптимизация GIF
+import svgo from 'imagemin-svgo'; // Оптимизация SVG
+import ttf2woff from 'gulp-ttf2woff';
+import ttf2woff2 from 'gulp-ttf2woff2';
+
+
+
+
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
+
+
+// Пути к файлам
+const paths = {
+  fonts: {
+    src: 'src/fonts/**/*.{ttf,woff,woff2}', // Форматы шрифтов
+    dest: 'dist/fonts/'
   },
-  src: {
-    indexHtml: 'source/index.html',
-    html: 'source/**/*.html',
-    style: {
-      nativeCss: 'source/style/',
-    },
-    fonts: 'source/fonts/',
-    script: 'source/**/*.js',
-    img: 'source/img/**/*',
+
+  styles: {
+    src: 'src/**/*.{css,scss}',
+    dest: 'dist/style/',
+    outputName: 'style-min.css'
   },
-  clean: 'build/**',
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'dist/js/'
+  },
+  images: {
+    src: 'src/img/**/*.{jpg,jpeg,png,gif,svg}', // Все форматы
+    dest: 'dist/img/'
+  },
+  html: {
+    src: 'src/*.html',
+    dest: 'dist/'
+  },
+  components: {
+    src: 'src/template/**/*.html' // Папка с частями HTML (header, footer и т.д.)
+  }
 };
 
-const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');        // для трансшпиляции
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');      // для объединения файлов
-const cssnano = require('gulp-cssnano');    // для минификации css-файлов
-const del = require('del');                 // zero client tool
-const env = require('gulp-env');            // переменные окружения
-const gulp = require('gulp');
-const gulpif = require('gulp-if');          // для минификации js-файлов
-const imagemin = require('gulp-imagemin');  // compress images
-const plumber = require('gulp-plumber');    // модуль для отслеживания ошибок
-const rigger = require('gulp-rigger');
-const sourcemaps = require('gulp-sourcemaps');
-const ttf2woff = require('gulp-ttf2woff');
-const ttf2woff2 = require('gulp-ttf2woff2');
-const uglify = require('gulp-uglify');      // для минификации js-файлов
-const eslint = require('gulp-eslint');
-const stylelint = require('gulp-stylelint');
+// Очистка папки dist
+function clean() {
+  return deleteAsync(['dist/*']);
+}
 
-env({
-  file: '.env',
-  type: 'ini',
-});
+// Компиляция SCSS в CSS + автопрефиксы + минификация
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat(paths.styles.outputName)) // Объединение в style-min.css
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(bs.stream());
+}
 
-/* zero client */
-gulp.task('clean', () => del(path.clean));
-
-/* сборка html */
-gulp.task('build-html', () => {
-  gulp.src(path.src.indexHtml)
-    .pipe(plumber())
-    .pipe(rigger())
-    .pipe(gulp.dest(path.build.html));
-});
-
-/* сборка стилей в один файл style-min.css */
-gulp.task('build-styles', () => {
-  gulp.src(path.src.style.nativeCss + '*.*')
-    .pipe(plumber())
-    .pipe(stylelint({
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
-    }))
-    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.init()))
-    .pipe(gulpif(process.env.PRODUCTION === 'switch-on', autoprefixer({
-      cascade: false,
-    })))
-    .pipe(concat('style-min.css'))
-    .pipe(gulpif(process.env.PRODUCTION === 'switch-on', cssnano()))
-    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.write()))
-    .pipe(gulp.dest(path.build.style));
-});
-
-/* сборка шрифтов */
-gulp.task('build-fonts', () => {
-  gulp.src(`${path.src.fonts}*.ttf`)
+// Копирование шрифтов
+function fonts() {
+  gulp.src(paths.fonts.src, { encoding: false, removeBOM: false })
     .pipe(ttf2woff())
-    .pipe(gulp.dest(path.src.fonts));
-  return gulp.src(`${path.src.fonts}*.ttf`)
+    .pipe(gulp.dest(paths.fonts.dest))
+
+
+  return gulp.src(paths.fonts.src, { encoding: false, removeBOM: false })
     .pipe(ttf2woff2())
-    .pipe(gulp.dest(path.src.fonts));
-});
+    .pipe(gulp.dest(paths.fonts.dest))
+    .pipe(bs.stream());
+}
 
-gulp.task('copy-fonts', () => {
-  gulp.src(`${path.src.fonts}*.*[^".css"]`)
-    .pipe(gulp.dest(path.build.fonts));
+// Объединение и минификация JS
+function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(concat('main-min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(bs.stream());
+}
 
-  gulp.src(`${path.src.fonts}*.css`)
-    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.init()))
-    .pipe(autoprefixer({
-      cascade: false,
+// Оптимизация изображений
+function images() {
+  return gulp.src(paths.images.src, { encoding: false })
+    .pipe(imagemin([
+      mozjpeg({ quality: 80 }), // Было 80 → стало 85 (менее агрессивно)
+      pngquant({ quality: [0.6, 0.8] }), // Было [0.6, 0.8] → стало [0.7, 0.9]
+      gifsicle({ optimizationLevel: 1 }), // Меньше сжатие для GIF
+      svgo({
+        plugins: [
+          { name: 'removeViewBox', active: false }, // Не удалять viewBox
+        ]
+      }) // Не удалять viewBox у SVG
+    ], {
+      verbose: true // Показывает детали оптимизации
     }))
-    .pipe(concat('fonts.css'))
-    .pipe(gulpif(process.env.PRODUCTION === 'switch-on', cssnano({
-      minifyFontValues: false,
-      discardUnused: false,
-    })))
-    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.write()))
-    .pipe(gulp.dest(path.build.fonts));
-});
+    .on('error', error => {
+      console.error('Image optimization error:', error);
+    })
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(bs.stream());
+}
 
-/* сборка скриптов в один файл *-min.js */
-gulp.task('build-scripts', () => {
-  gulp.src(path.src.script)
-    .pipe(plumber())
-    .pipe(eslint('./.eslintrc'))
-    .pipe(eslint.format())
-    .pipe(rigger())
-    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.init()))
-    .pipe(concat('scripts-min.js'))
-    .pipe(gulpif(process.env.BABEL === 'switch-on', babel({
-      presets: [
-        '@babel/env',
-      ],
-    })))
-    .pipe(gulpif(process.env.PRODUCTION === 'switch-on', uglify()))
-    .pipe(gulpif(process.env.SOURCEMAPS === 'switch-on', sourcemaps.write()))
-    .pipe(gulp.dest(path.build.script));
-});
 
-// compress images
-gulp.task('build-images', () => {
-  gulp.src(path.src.img)
-    .pipe(imagemin())
-    .pipe(gulp.dest(path.build.img));
-});
+// Сборка HTML с вставкой компонентов (gulp-file-include)
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(fileInclude({
+      prefix: '@@', // Префикс для вставки (например, @@include('header.html'))
+      basepath: '@file' // Относительные пути от текущего файла
+    }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(bs.stream());
+}
 
-gulp.task(
-  'default', [
-    'build-fonts',
-    'copy-fonts',
-    'build-html',
-    'build-styles',
-    'build-scripts',
-    'build-images',
-    'browser-sync',
-  ]);
-
-// build for prod
-gulp.task(
-  'build', [
-    'clean',
-    'build-fonts',
-    'copy-fonts',
-    'build-html',
-    'build-styles',
-    'build-scripts',
-    'build-images',
-  ]);
-
-// browser-sync
-gulp.task('browser-sync', () => {
-  browserSync.init({
+// Локальный сервер и отслеживание изменений
+function serve() {
+  bs.init({
     server: {
-      baseDir: './build/',
-    },
-    port: 3006,
+      baseDir: './dist'
+    }
   });
-  gulp.watch(path.src.fonts, [
-    'watch-fonts',
-  ]);
-  gulp.watch(path.src.html, [
-    'watch-html',
-  ]);
-  gulp.watch(path.src.style.nativeCss + '*.*', [
-    'watch-styles',
-  ]);
-  gulp.watch(path.src.script, [
-    'watch-scripts',
-  ]);
-  gulp.watch(path.src.img, [
-    'watch-images',
-  ]);
-});
 
-// watchers
-gulp.task('watch-fonts', [
-  'build-fonts',
-  'copy-fonts',
-], () => browserSync.reload());
-gulp.task('watch-html', [
-  'build-html',
-], () => browserSync.reload());
-gulp.task('watch-styles', [
-  'build-styles',
-], () => browserSync.reload());
-gulp.task('watch-scripts', [
-  'build-scripts',
-], () => browserSync.reload());
-gulp.task('watch-images', [
-  'build-images',
-], () => browserSync.reload());
+
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.fonts.src, fonts);
+  gulp.watch(paths.images.src, images);
+  gulp.watch([paths.html.src, paths.components.src], html);
+  gulp.watch(paths.html.src).on('change', bs.reload);
+}
+
+
+// Основные задачи
+const build = gulp.series(clean, gulp.parallel(styles, scripts, images, fonts, html));
+const dev = gulp.series(build, serve);
+
+// Экспорт задач
+export {
+  clean,
+  styles,
+  scripts,
+  images,
+  fonts,
+  html,
+  build,
+  serve as dev
+};
+
+export default dev;
