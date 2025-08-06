d56c705d4b1f344536b4f1e3f9e51d762370c5fd
Anton Tkachuk <tkachuk.ant@mail.ru>
2025-08-06 03:01:37 +0300

    update

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,3 +1,27 @@
+function disableScroll() {
+  const width = document.body.offsetWidth
+  const scrollTop = window.pageYOffset ||  document.documentElement.scrollTop;
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  document.body.style.position = 'fixed';
+  if(scrollTop) {
+    document.body.style.top = `-${scrollTop}px`;
+  }
+  document.body.style.left = `-${scrollLeft}px`;
+  document.body.style.width = `${width}px`;
+}
+
+function enableScroll() {
+  const scrollTop = parseInt(document.body.style.top || '0');
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.left = '';
+  document.body.style.width = '';
+  window.scrollTo(0,(scrollTop * -1));
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   const sliderWrapper = document.querySelector('.slider__wrapper');
   const sliderLent = document.querySelector('.slider__lent');
@@ -5,29 +29,46 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   // Обработчик события прокрутки
-  function handleScroll() {
+  function handleScroll(event) {
     let lentWidth = sliderLent.scrollWidth;
+    const minOffset = 0
     const maxOffset = lentWidth - sliderWrapper.offsetWidth
 
-    const sliderTop = sliderLent.getBoundingClientRect().top
-    const sliderBottom = sliderLent.getBoundingClientRect().bottom
-    const sliderHeight = sliderLent.getBoundingClientRect().height
+    const sliderTop = sliderWrapper.getBoundingClientRect().top
+    const sliderBottom = sliderWrapper.getBoundingClientRect().bottom
+    const sliderHeight = sliderWrapper.getBoundingClientRect().height
 
     if (sliderTop > 0 && (sliderBottom - windowHeight) < 0) {
-      const height = windowHeight - sliderHeight
-      const percent = ((height - sliderTop) / height)
-      const offset = maxOffset * percent
-      sliderLent.style.transform = `translateX(-${offset}px)`;
-    } else if (sliderTop > 0) {
-      sliderLent.style.transform = `translateX(-${0}px)`;
-    } else {
-      sliderLent.style.transform = `translateX(-${maxOffset}px)`;
+      disableScroll()
+
+
+      let transform = 0
+      if(sliderLent.style.transform) {
+        transform = Number(sliderLent.style.transform.match(/\(([^)]+)px\)/)[1])
+      }
+
+      let direction = 1
+      if (event.deltaY < 0) {
+        direction = -1
+      }
+
+      const currentOffset = transform
+      const newOffset = currentOffset + 150 * -direction
+
+      sliderLent.style.transform = `translateX(${newOffset}px)`;
+
+      if(event.deltaY > 0 && newOffset * -1 > maxOffset) {
+        sliderLent.style.transform = `translateX(-${maxOffset}px)`;
+        enableScroll()
+      } else if(event.deltaY < 0 && newOffset * -1 < minOffset) {
+        sliderLent.style.transform = `translateX(-${minOffset}px)`;
+        enableScroll()
+      }
     }
   }
 
-  // Добавляем обработчики событий
-  window.addEventListener('scroll', handleScroll);
+  const throttled = throttle(handleScroll)
 
-  // Вызываем сразу при загрузке
-  handleScroll();
+  // Добавляем обработчики событий
+  document.addEventListener('wheel', throttled);
 });
---
+++ src/scripts/throttle.js
@@ -0,0 +1,28 @@
+function throttle(
+  callback,
+  delay = 200
+) {
+  let isWaiting = false
+  let savedArgs= null
+  let savedThis = null
+
+  return function wrapper ( ...args) {
+    if (isWaiting) {
+      savedArgs = args
+      savedThis = this
+      return
+    }
+
+    callback.apply(this, args)
+
+    isWaiting = true
+    setTimeout(() => {
+      isWaiting = false
+      if (savedThis) {
+        wrapper.apply(savedThis, savedArgs )
+        savedThis = null
+        savedArgs = null
+      }
+    }, delay)
+  }
+}
